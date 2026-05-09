70af8c58b67319bb4e1040bc760637e0635b74dd
chanaparose <rosepupiam@gmail.com>
2026-05-09 21:52:13 +0700

    banner

--- src/app/pages/explore/Explore.desktop.tsx
+++ src/app/pages/explore/Explore.desktop.tsx
@@ -473,7 +473,7 @@ export function ExploreDesktop({
           <ImageWithFallback
             src="assets/tryly_banner_slim.png"
             alt="Tryly banner"
-            className="block h-full w-full object-cover object-[center_calc(50%+10px)]"
+            className="block h-full w-full object-cover object-[center_45%]"
           />
         </section>
 
