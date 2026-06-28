5d4bb676120fe6b88f2ac0724417d0636f8f4326
chanaparose <rosepupiam@gmail.com>
2026-06-28 19:51:04 +0700

    bg

--- src/app/pages/order-detail/OrderDetail.desktop.tsx
+++ src/app/pages/order-detail/OrderDetail.desktop.tsx
@@ -3,10 +3,8 @@ import { OrderDetailMobile } from '@/pages/order-detail/OrderDetail.mobile';
 
 export function OrderDetailDesktop() {
   return (
-    <div className='hidden lg:block'>
-      <div className='mx-auto max-w-[1600px] px-8 py-5 2xl:px-10'>
-        <OrderDetailMobile />
-      </div>
+    <div className='hidden w-full lg:block'>
+      <OrderDetailMobile />
     </div>
   );
 }
